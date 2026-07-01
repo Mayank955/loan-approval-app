0727d5ec596a2fc42588a508b568a4526c1402e0
Mayank Prajapati <nemsinghprajapati606@gmail.com>
2026-07-01 21:22:11 +0530

    Initial commit

--- Loan_Approval.py
+++ Loan_Approval.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix
-import streamlit as s
 
 
 data = {
@@ -70,33 +69,6 @@ plt.ylabel("Actual")
 plt.show()
 
 
-
-
-s.title(":blue[Loan Approval Predictor:]")
-
-income = s.slider("Income : ", 20000, 100000)
-credit_score = s.slider("Credit Score : ", 300, 850)
-loan_amount = s.slider("Loan Amount : ", 10000, 500000)
-
-prediction = model.predict([[income, credit_score, loan_amount]])
-
-# Success
-if prediction[0] == 1:
-    s.success("Loan Approved")
-else:
-    s.error("Loan Rejected")
-
-# Write
-if prediction[0] == 1:
-    s.write("Prediction : Pass")
-else:
-    s.write("Prediction : Fail")
-
-
-s.title("_Streamlit_ is :blue[cool] :sunglasses:")
-
 joblib.dump(model, "loan_model.pkl")
 print("\nModel saved successfully as loan_model.pkl")
 
-# cd "C:\Users\nemsi\OneDrive\Desktop\Python_Learn\JASIQ_LABS_PY\Phase 4\Capstone Projects"
-# streamlit run app.py
